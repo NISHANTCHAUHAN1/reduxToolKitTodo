a1836de61482b77750907c1740268b498570adec
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-05-29 16:40:05 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+    <h1> Learn about Redux ToolKit</h1>
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
